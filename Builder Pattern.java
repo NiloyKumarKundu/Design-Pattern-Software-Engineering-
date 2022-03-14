92aa306be7cc0ac6517304ebc764cd965e4626e6
Niloy Kumar Kundu <nkundu191071@bscse.uiu.ac.bd>
2022-03-14 21:45:58 +0600

    Add files via upload

---
+++ Builder Pattern.java
@@ -0,0 +1,73 @@
+public class Meal {
+    private String appetizer;
+    private String mainDish;
+    private String salad;
+    private String desert;
+    private String drink;
+
+    private Meal(Builder builder) {
+        this.appetizer = builder.appetizer;
+        this.desert = builder.desert;
+        this.drink = builder.drink;
+        this.mainDish = builder.mainDish;
+        this.salad = builder.salad;
+    }
+
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+
+    public static class Builder {
+        private String appetizer;
+        private String mainDish;
+        private String salad;
+        private String desert;
+        private String drink;
+
+        public Builder mainDish(String mainDish) {
+            this.mainDish = mainDish;
+            return this;
+        }
+
+        public Builder salad(String salad) {
+            this.salad = salad;
+            return this;
+        }
+
+        public Builder desert(String desert) {
+            this.desert = desert;
+            return this;
+        }
+
+        public Builder drink(String drink) {
+            this.drink = drink;
+            return this;
+        }
+
+        public Builder appetizer(String appetizer) {
+            this.appetizer = appetizer;
+            return this;
+        }
+
+        public Meal build() {
+            return new Meal(this);
+        }
+    }
+
+    public static void main(String[] args) {
+        Builder b = new Builder().salad("This salad is too good.")
+                .desert("I love to eat desert.")
+                .drink("I am drinking water.")
+                .appetizer("I don't know that is appetizer.")
+                .mainDish("Rice is our main dish.");
+
+        System.out.println(b.build().drink);
+        System.out.println(b.build().desert);
+        System.out.println(b.build().salad);
+        System.out.println(b.build().appetizer);
+        System.out.println(b.build().mainDish);
+    }
+}
+
